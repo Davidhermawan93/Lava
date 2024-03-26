b61a433fd761a0b92c5bc584d6b44be077dbcca8
Davidhermawan93 <90891883+Davidhermawan93@users.noreply.github.com>
2024-03-26 20:57:17 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-033adac7-63c4-4de2-b23e-b446ab32d256/)
 
 
 echo "$curl_output" >> Update.tsx
